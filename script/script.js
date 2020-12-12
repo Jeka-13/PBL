de73c9cb9bf690fb5cb825809a33514202570378
Jeka-13 <70950690+Jeka-13@users.noreply.github.com>
2020-12-12 23:49:57 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,14 +1,37 @@
+let check;
+
 function readFile(input) {
-    document.getElementById("download-label").style.display="block";
     let data = new FormData();
 
     data.append('file', input.files[0]);
+    check = input.files[0];
 
     fetch('http://localhost:8080/sound', { //here add your url
         method: 'POST',
         body: data
-    }).then(r => console.log(r))
-
+    }).then(r => {
+        console.log(r);
+        document.getElementById("download-label").style.display="block";
+        }
+    )
 }
 
+let button = document.getElementById("download-button");
+
+button.addEventListener("click", function (e) {
+    let filename = "output.txt";
+
+    download(check, filename)
+
+});
+
+function download(check, filename) {
+   let element = document.createElement('a');
+   element.setAttribute('href', 'data:text/plain;charset=utf-8,'+ encodeURIComponent(check));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+   document.body.appendChild(element);
+   element.click();
+   document.body.removeChild(element);
+}
 
