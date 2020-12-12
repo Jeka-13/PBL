562a6240ead45607ed2d4a3210cb3dcaf1eb7e24
Jeka-13 <70950690+Jeka-13@users.noreply.github.com>
2020-12-12 18:13:05 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,29 @@
+let input = document.querySelector('input[type="file"]');
+
+let data = new FormData();
+
+function readFile(input) {
+    let file = input.files[0];
+
+    let reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload= function (e){
+        data.append('file', input.files[0]);
+        fetch('http://localhost:63342/PBL/index.html?_ijt=msk3b97oo87hpnjtb81g9ib82d', { //here add your url
+            method: 'POST',
+            body: data
+        }).then(success => console.log(success)
+        ).catch(error=> console.log(error));
+
+    };
+
+    reader.onerror = function() {
+        console.log(reader.error);
+    };
+
+}
+
+
+
