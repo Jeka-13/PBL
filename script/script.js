016d08a08124bfbf38684d16bce1662f96f958ee
Jeka-13 <70950690+Jeka-13@users.noreply.github.com>
2020-12-21 21:03:24 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,8 @@
 var check;
+let button = document.getElementById("download-button");
+let button2 = document.getElementById("upload-button");
+let waitText = document.getElementById('display-wait');
+let formatText = document.getElementById("info-format");
 
 function readFile(input) {
     let data = new FormData();
@@ -6,18 +10,24 @@ function readFile(input) {
     data.append('file', input.files[0]);
     check = input.files[0];
 
+    button2.style.display = 'none';
+    waitText.style.display='block';
+    formatText.style.display = 'none';
+
     fetch('http://localhost:8080/sound', { //here add your url
         method: 'POST',
         body: data
     }).then(response => {
             response.text().then(function(result){
+                waitText.style.display = 'none';
+                button.style.display = 'block';
+                formatText.style.display = 'block';
                 check = result
             })
         }
     )
 }
 
-let button = document.getElementById("download-button");
 
 button.addEventListener("click", function (e) {
     let filename = "output.mid";
@@ -26,7 +36,7 @@ button.addEventListener("click", function (e) {
 
 function download(check, filename) {
     let element = document.createElement('a');
-    console.log(check)
+    console.log(check);
     element.setAttribute('href', 'data:text/plain;base64,' + check);
     element.setAttribute('download', filename);
     element.style.display = 'none';
